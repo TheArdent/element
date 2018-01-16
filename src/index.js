3bf6de819c5aea1a94e90506dc5d0af149c4a266
Oleksii Yaryi <oleksii.yaryi@gmail.com>
2018-01-16 12:17:22 +0200

    publish

--- src/index.js
+++ src/index.js
@@ -171,7 +171,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 };
 
 module.exports = {
-  version: '2.1.10000014',
+  version: '2.1.10000015',
   locale: locale.use,
   i18n: locale.i18n,
   install,
